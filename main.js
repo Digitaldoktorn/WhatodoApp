b094600e6fe01d3fd6b228d9c66dd5ca90a84b8e
Anders Eriksson <digitaldoktorn@gmail.com>
2019-09-04 06:28:48 +0200

    commit

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-const list = document.querySelector('#task-list ul')
+document.addEventListener('DOMContentLoaded', function(){
+    const list = document.querySelector('#task-list ul')
 
-// tabbed content
-const tabs = document.querySelector('.tabs')
-const panels = document.querySelectorAll('.panel')
-tabs.addEventListener('click', function(e) {
-    if(e.target.tagName == 'LI'){
-        const targetPanel = document.querySelector(e.target.dataset.target)
-        panels.forEach(function(panel){
-            if(panel == targetPanel){
-                panel.classList.add('active')
-            } else {
-                panel.classList.remove('active')
-            }
-        })
-    }
-})
+    // delete tasks (using event bubbling)
+    list.addEventListener('click', function(e){
+        if(e.target.className == 'delete'){
+            const li = e.target.parentElement; // this is the parent element of the button
+            list.removeChild(li)
+        }
+    })
 
-// delete tasks
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li)
-    }
+    // add books
+    const addForm = document.forms['add-task']
+    addForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        const value = addForm.querySelector('input[type="text"]').value
+
+        // create elements
+        const li = document.createElement('li')
+        const taskName = document.createElement('span')
+        const deleteBtn = document.createElement('span')
+
+        // add content
+        taskName.textContent = value
+        deleteBtn.textContent = 'delete'
+
+        // add classes
+        taskName.classList.add('name')
+        deleteBtn.classList.add('delete')
+
+
+        // append to DOM - the order matters
+        li.appendChild(taskName)
+        li.appendChild(deleteBtn)
+        list.appendChild(li)
+
+    })
+
+    // hide tasks
+    const hideBox = document.querySelector('#hide')
+    hideBox.addEventListener('change', function(e){
+        if(hideBox.checked){
+            list.style.display = 'none'
+        } else {
+            list.style.display = 'initial'
+        }
+    })
+    
+    
 })
